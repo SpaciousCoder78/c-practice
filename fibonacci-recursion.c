9212c1f1fc76447d7a50526f84e351a1d504e3c6
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2023-12-22 18:21:18 +0530

    Create fibonacci-recursion.c

---
+++ fibonacci-recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int fibonacci(int num){
+    if(num==0){
+        return 0;
+    }
+    else if(num==1){
+        return 1;
+    }
+    else{
+        return fibonacci(num-1)+ fibonacci(num-2);
+    }
+}
+int main(){
+    int num;
+    printf("Enter no of elements ");
+    scanf("%d",&num);
+    for(int i =0;i<num;i++){
+        printf("%d", fibonacci((i)));
+    }
+}
