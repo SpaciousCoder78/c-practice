c0a35b9b5b14c0c0cc2b9a95640714168ed1d948
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2023-12-28 18:45:30 +0530

    Create number_pattern.c

---
+++ number_pattern.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//program to print a pattern of
+/*
+ * 00000
+ * 11111
+ * 22222
+ * 33333
+ * 44444
+ */
+int main(){
+    int i,j;
+    for(i=0;i<=4;i++){
+        for(j=0;j<=4;j++){
+            printf("%d",i);
+
+        }
+        printf("\n");
+    }
+}
+
